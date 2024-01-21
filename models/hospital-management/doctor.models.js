c7e20860283befc339031fd3f2d653fcb8d7a0c1
raipratik <pratikkumar399@gmail.com>
2024-01-22 01:24:42 +0530

    done with hospital management

--- models/hospital-management/doctor.models.js
+++ models/hospital-management/doctor.models.js
@@ -1,23 +1,27 @@
 import mongoose from "mongoose";
 
 const doctorSchema = new mongoose.Schema({
-    name : {
-        type :String,
-        required : true
+    name: {
+        type: String,
+        required: true
     },
-    salary : {
-        type : String,
-        required : true
+    salary: {
+        type: String,
+        required: true
     },
-    qualification :{
-        type :String,
-        required : true
+    qualification: {
+        type: String,
+        required: true
     },
-    experienceInYears : {
-        type : Number,
-        required : true
-    }
-} , {timestamps : true});
+    experienceInYears: {
+        type: Number,
+        default: 0
+    },
+    worksinHospital: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Hospital'
+    }]
+}, { timestamps: true });
 
 export const Doctor = mongoose.model(
     'Doctor',
--- models/hospital-management/hospital.models.js
+++ models/hospital-management/hospital.models.js
@@ -1,6 +1,33 @@
 import mongoose from "mongoose";
 
-const hospitalSchema = new mongoose.Schema({} , {timestamps : true});
+const hospitalSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    addressLine1: {
+        type: String,
+        required: true
+    },
+    addressLine2: {
+        type: String
+    },
+    city: {
+        type: String,
+        required: true
+    },
+    pincode: {
+        type: String,
+        required: true
+    },
+    contactNumber: {
+        type: Number
+    },
+    doctors: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Doctor'
+    }]
+}, { timestamps: true });
 
 export const Hospital = mongoose.model(
     'Hospital',
